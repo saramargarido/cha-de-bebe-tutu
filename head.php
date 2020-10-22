163430633346dedfc4c816baff3f57fcfec2ce3a
saramargarido <saramargarido@gmail.com>
2020-10-22 00:04:01 -0300

    content

---
+++ head.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chá de bebê do Tutu</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="app.js" defer></script>
+</head>
+<body>
+  <header>
+    <div class="word">
+        <span>A</span>
+        <span>R</span>
+        <span>T</span>
+        <span>H</span>
+        <span>U</span>
+        <span>R</span>
+    </div>
+<div class="menu">
+  <ul>
+    <li>
+      <span class="fish-button">
+      <a href="#">
+        <img src="img/fish-4.svg" alt="">
+        <p>Tutu</p>
+      </a>
+      </span>
+    </li>
+    <li>
+      <span class="fish-button">
+        <a href="#">
+          <img src="img/fish-17.svg" alt="">
+          <p>Tutu</p>
+        </a>
+      </span>
+    </li>
+    <li>
+      <span class="fish-button">
+        <img src="img/fish-5.svg" alt="">
+        <a href="#">
+          <p>Tutu</p>
+        </a>
+      </span>
+    </li>
+  </ul>
+</div>
+</header>
+
